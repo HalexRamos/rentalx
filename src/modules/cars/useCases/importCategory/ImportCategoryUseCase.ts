6989ea924808bd302a4771ca17c51aba49c26192
HalexRamos <halexramos96@gmail.com>
2021-06-04 16:43:54 -0300

    Usuario

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import csvParse from 'csv-parse';
+import { inject, injectable } from 'tsyringe';
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IImportCategory {
@@ -7,10 +8,14 @@ interface IImportCategory {
   description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository,
+  ) {}
 
-  loadCategories(file: Express.Multer.File): Promise<IImportCategory> {
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
       const categories: IImportCategory[] = [];
@@ -22,18 +27,16 @@ class ImportCategoryUseCase {
       parseFile
         .on('data', async line => {
           const [name, description] = line;
-          categories.push({
-            name,
-            description,
-          });
+
+          categories.push({ name, description });
         })
         .on('end', () => {
           fs.promises.unlink(file.path);
           resolve(categories);
         })
-        .on('error', err => {
-          reject(err);
-        });
+        .on('error', err => reject(err));
+
+      return categories;
     });
   }
 
@@ -43,10 +46,10 @@ class ImportCategoryUseCase {
     categories.map(async category => {
       const { name, description } = category;
 
-      const existCategory = this.categoriesRepository.findByName(name);
+      const existCategory = await this.categoriesRepository.findByName(name);
 
       if (!existCategory) {
-        this.categoriesRepository.create({
+        await this.categoriesRepository.create({
           name,
           description,
         });
