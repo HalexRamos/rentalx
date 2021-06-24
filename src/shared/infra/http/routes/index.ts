25866fb50cc5ad08cd2f9b13d6d74c2e9673ffcc
HalexRamos <halexramos96@gmail.com>
2021-06-24 01:42:41 -0300

    Testes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
 import { authenticateRoutes } from './authenticate.routes';
+import { carsRoutes } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { passwordRoutes } from './password.routes';
 import { rentalRoutes } from './rental.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
-import { carsRoutes } from './cars.routes';
 
 const router = Router();
 
 router.use(authenticateRoutes);
+router.use('/password', passwordRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/cars', carsRoutes);
 router.use('/rentals', rentalRoutes);
